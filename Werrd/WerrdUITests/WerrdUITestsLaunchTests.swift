d5943f5eb763f2773071ee97470afd6b4feedc97
Saurabh D <d27saurabh@gmail.com>
2023-04-12 20:20:22 -0400

    Created the initial Werrd Xcode project

---
+++ Werrd/WerrdUITests/WerrdUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WerrdUITestsLaunchTests.swift
+//  WerrdUITests
+//
+//  Created by Saurabh Deshpande on 4/12/23.
+//
+
+import XCTest
+
+final class WerrdUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
